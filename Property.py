02bffe11e435d9b6a72e14f7dbf0517a8c5a79ec
Lujun Jian <ljian@smith.edu>
2016-05-01 22:31:37 -0400

    Update consumer and property classes

--- Property.py
+++ Property.py
@@ -1,9 +1,11 @@
 class Property:
-    def __init__(self, id, star, cost):
+    def __init__(self, id, star, cost, availability):
         self.id = id
         self.star = star
         self.cost = cost
         self.priceList = []
+        self.availability = availability
+        self.profit = 0
         
     def set_priceList(self, coeffAList, coeffBList, maxSlot):
         #price = cost*a + b
@@ -12,3 +14,7 @@ class Property:
 
     def get_priceList(self):
         return self.priceList
+
+    def transcation_occur(self, price):
+        self.profit += price - cost
+        self.availability -= 1
